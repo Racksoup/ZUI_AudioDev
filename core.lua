ccc20e989ae92508d45e6626d9cf3b835c5a7d28
Racksoup <rack.connor1@gmail.com>
2022-01-26 23:06:55 -0500

    Scrollbar working! needs finetuning

--- core.lua
+++ core.lua
@@ -20,7 +20,13 @@ function Config:CreateButton(point, relativeFrame, relativePoint, yOffset, width
     return btn;
 end
 
----------------------------------
+function MyScrollBar_Update()
+    FauxScrollFrame_Update(UIConfig.MyScrollBar,50,5,16);
+      -- 50 is max entries, 5 is number of lines, 16 is pixel height of each line
+      DEFAULT_CHAT_FRAME:AddMessage("We're at "..FauxScrollFrame_GetOffset(UIConfig.MyScrollBar));
+  end
+
+-------------Frames--------------
 local UIConfig = CreateFrame("Frame", "ZUIAudioFrame", UIParent, "BasicFrameTemplateWithInset");
 UIConfig:SetSize(300, 420); -- width, height
 UIConfig:SetPoint("CENTER");
@@ -35,17 +41,27 @@ UIConfig.title:SetFontObject("GameFontHighlight");
 UIConfig.title:SetPoint("LEFT", UIConfig.TitleBg, "LEFT", 6, 1);
 UIConfig.title:SetText("ZUI Audio Dev");	
 
-UIConfig.ScrollFrame = CreateFrame("ScrollFrame", "ZUIAudioScrollFrame", UIConfig, "FauxScrollFrameTemplate");
-UIConfig.ScrollFrame:SetScript("OnVerticalScroll", function(self, offset)
-	FauxScrollFrame_OnVerticalScroll(self, offset, BUTTON_HEIGHT, update)
-end)
-UIConfig.ScrollFrame:SetPoint("TOPLEFT", "ZUIAudioFrame", "TOPLEFT", 4, -8);
-UIConfig.ScrollFrame:SetPoint("BOTTOMRIGHT", "ZUIAudioFrame", "BOTTOMRIGHT", -3, 4);
+UIConfig.MyScrollBar = CreateFrame("ScrollFrame", "ZUIAudioMyScrollBar", UIConfig, "UIPanelScrollFrameTemplate");
+UIConfig.MyScrollBar:SetPoint("TOPLEFT", "ZUIAudioFrame", "TOPLEFT", 4, -8);
+UIConfig.MyScrollBar:SetPoint("BOTTOMRIGHT", "ZUIAudioFrame", "BOTTOMRIGHT", -3, 4);
 
-UIConfig.murlocBtn = Config:CreateButton("CENTER", UIConfig, "TOP", -70, 140, "Murlocs", "sound/creature/murloc/mmurlocaggroold.ogg");
+UIConfig.scrollChild = CreateFrame("Frame");
+UIConfig.MyScrollBar:SetScrollChild(UIConfig.scrollChild);
+UIConfig.scrollChild:SetWidth(200);
+UIConfig.scrollChild:SetHeight(1);
+
+local title = UIConfig.scrollChild:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
+title:SetPoint("TOP")
+title:SetText("MyAddOn")
+
+local footer = UIConfig.scrollChild:CreateFontString("ARTWORK", nil, "GameFontNormal")
+footer:SetPoint("TOP", 0, -5000)
+footer:SetText("This is 5000 below the top, so the scrollChild automatically expanded.")
+
+UIConfig.murlocBtn = Config:CreateButton("CENTER", UIConfig.MyScrollBar, "TOP", -70, 140, "Murlocs", "sound/creature/murloc/mmurlocaggroold.ogg");
 UIConfig.dieingManBtn = Config:CreateButton("CENTER", UIConfig.murlocBtn, "TOP", -70, 140, "DieingMan", "sound/character/playerexertions/humanmalefinal/humanmalemaindeatha.ogg");
 UIConfig.flagTakenBtn = Config:CreateButton("CENTER", UIConfig.dieingManBtn, "TOP", -70, 180, "Flag Taken", "sound/spells/pvpflagtaken.ogg");
-UIConfig.Zelda= Config:CreateButton("CENTER", UIConfig.flagTakenBtn, "TOP", -70, 140, "ZELDA!", "Interface/AddOns/ZUI_AudioDev/Sounds/TheLegendaryHero-WindWaker.ogg");
+UIConfig.Zelda = Config:CreateButton("CENTER", UIConfig.flagTakenBtn, "TOP", -70, 140, "ZELDA!", "Interface/AddOns/ZUI_AudioDev/Sounds/TheLegendaryHero-WindWaker.ogg");
 
 
 
